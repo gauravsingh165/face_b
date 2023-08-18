9f8618d7522d3935358ec93532593b540e794419
Gaurav Singh <gaurav165ongraph@gmail.com>
2023-08-18 14:03:53 +0530

    add devise gem and make controller and api routes

---
+++ db/migrate/20230818070955_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :dob
+      t.string :address
+      t.integer :zip_code
+
+      t.timestamps
+    end
+  end
+end
