b74acb03fe0de513cd2717febe960858f70b5a80
Gaurav Singh <gaurav165ongraph@gmail.com>
2023-08-18 17:19:13 +0530

    add the post controller actions

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,3 +1,24 @@
 class Api::V1::PostsController < ApplicationController
+  #  before_action :find_user
 
+      def create
+        post = @user.posts.build(post_params)
+
+        if post.save
+          render json: post, status: :created
+        else
+          render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      # def find_user
+      #   @user = User.find(params[:user_id])
+      # end
+
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
+	
 end
