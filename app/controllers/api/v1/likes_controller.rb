8eb9be8d53292620a44b3fdf8fdf7641a6b350ee
Gaurav Singh <gaurav165ongraph@gmail.com>
2023-08-18 18:57:34 +0530

    add the like  controller actions

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,3 +1,33 @@
 class Api::V1::LikesController < ApplicationController
+    before_action :find_post
 
+    def create
+        debugger
+      like = @post.likes.build(user: current_user)
+      
+      if like.save
+        render json: { message: 'Post liked successfully' }, status: :created
+      else
+        render json: { errors: like.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      like = current_user.likes.find_by(post: @post)
+      
+      if like
+        like.destroy
+        render json: { message: 'Like removed successfully' }, status: :ok
+      else
+        render json: { message: 'Like not found' }, status: :not_found
+      end
+    end
+
+    private
+
+    def find_post
+        debugger
+      @post = current_user.posts.find(params[:post_id])
+    end
+	
 end
